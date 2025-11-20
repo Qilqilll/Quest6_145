7525a1bbf96e2757eeebed828d9b30fca0184034
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-20 16:50:37 +0700

    composable tampil siswa

--- app/src/main/java/com/example/praktikum8/view/TampilanData.kt
+++ app/src/main/java/com/example/praktikum8/view/TampilanData.kt
@@ -5,4 +5,9 @@ import androidx.compose.ui.res.stringResource
 import com.example.praktikum8.R
 import com.example.praktikum8.model.Siswa
 
-
+@Composable
+fun TampilSiswa(
+    // edit 1 : parameter statusUiSiswa
+    statusUiSiswa: Siswa,
+    onBackButtonClicked:() -> Unit
+)
