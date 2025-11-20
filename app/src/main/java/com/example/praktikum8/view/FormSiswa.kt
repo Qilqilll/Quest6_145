5de375de8ee0a6afd686ab784de8ed7127b867e7
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-20 16:27:54 +0700

    isi ruang

--- app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
@@ -57,3 +57,8 @@ fun FormSiswa(
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
