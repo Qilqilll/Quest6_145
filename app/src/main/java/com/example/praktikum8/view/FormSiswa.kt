8324277398160a459a494aabf32a21ab953da7fc
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-20 16:26:13 +0700

    var dan val nama alamat gender data

--- app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
@@ -39,3 +39,7 @@ fun FormSiswa(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     modifier:Modifier = Modifier
 ){
+    var txtNama by rememberSaveable {mutableStateOf("")}
+    var txtAlamat by remember {mutableStateOf("")}
+    var txtGender by remember {mutableStateOf("")}
+    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
