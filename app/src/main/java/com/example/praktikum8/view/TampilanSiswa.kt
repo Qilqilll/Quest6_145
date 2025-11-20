02f5260c8a7a02c62e1f2e8f3078f04b120d3a1e
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-20 16:55:49 +0700

    import tampilan siswa

---
+++ app/src/main/java/com/example/praktikum8/view/TampilanSiswa.kt
@@ -0,0 +1,26 @@
+package com.example.praktikum8.view
+
+
+import androidx.compose.foundation.background // Import ini
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.praktikum8.R
+import com.example.praktikum8.model.Siswa
